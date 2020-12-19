6541d1ddae651a764916c848baa76196a7ff6100
chamin96 <chaminmax@gmail.com>
2020-12-20 01:18:41 +0530

    improvements to run_video.py and run_image.py

--- run_image.py
+++ run_image.py
@@ -7,19 +7,32 @@ from src import model
 from src import util
 from src.body import Body
 
+import time
+
 body_estimation = Body('model/body_pose_model.pth')
 
 img = 'images/wl-man-3.png'
 frame = cv2.imread(img)  # B,G,R order
+
+start = time.time()
+
 candidate, subset = body_estimation(frame)
 canvas = copy.deepcopy(frame)
+
 print((subset))
 print((candidate))
+
 canvas = util.draw_bodypose(canvas, candidate, subset)
 
-cv2.imshow('Preview', canvas)
-cv2.waitKey(0)
+end = time.time()
+print("Elapsed time: {} seconds".format(end - start))
 
-# plt.imshow(canvas[:, :, [2, 1, 0]])
-# plt.axis('off')
-# plt.show()
+# cv2.imshow('Preview', canvas)
+# cv2.waitKey(0)
+
+plt.imshow(canvas[:, :, [2, 1, 0]])
+plt.axis('off')
+plt.show()
+
+
+
--- run_video.py
+++ run_video.py
@@ -7,23 +7,33 @@ from src import model
 from src import util
 from src.body import Body
 
+import time
+
 body_estimation = Body('model/body_pose_model.pth')
 
-capture = cv2.VideoCapture('videos/men85kg_0017.mp4')
+input_filename = 'snatch_side_view'
+capture = cv2.VideoCapture("videos/{}.mp4".format(input_filename))
 
 # Default resolutions of the frame are obtained.The default resolutions are system dependent.
 # We convert the resolutions from float to integer.
 frame_width = int(capture.get(3))
 frame_height = int(capture.get(4))
 
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('results/output.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+output_filename = "{}-output".format(input_filename)
+
+# Define the codec and create VideoWriter object.
+out = cv2.VideoWriter("results/{}.avi".format(output_filename), cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
+frame_count = 0
+start = time.time()
 
 while True:
     isTrue, frame = capture.read()
 
     if isTrue == True:
-        print('Processing...')
+        frame_count += 1
+        print('Processing %dth frame...' %frame_count)
+
         candidate, subset = body_estimation(frame)
         canvas = copy.deepcopy(frame)
         canvas = util.draw_bodypose(canvas, candidate, subset)
@@ -46,5 +56,8 @@ capture.release()
 out.release()
 
 cv2.destroyAllWindows()
+
+end = time.time()
+print("Elapsed time: {} seconds".format(end - start))
 
 print('Done')
